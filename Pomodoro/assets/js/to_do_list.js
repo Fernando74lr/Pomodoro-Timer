548c34562a62cef9f43f572ee3f320a504455cc9
Fernando <flopezramirez@hotmail.com>
2020-05-13 23:54:22 -0500

    Changes in kanban mode and added dragg&dropp demo

--- Pomodoro/assets/js/to_do_list.js
+++ Pomodoro/assets/js/to_do_list.js
@@ -2,7 +2,8 @@
 // Variables
 let id_tasks = 0,
     id_tasks_2 = 0,
-    tasks = 0;
+    tasks = 0,
+    tasks_2 = 0;
 
 // Tag variables
 let input_task = $("#input_task"),
@@ -26,10 +27,10 @@ function addTask() {
             tasks++;
             $("#task_list").append(`
                 <li>
-                    <div class='tasks' style="height:${height_1}px" id="task_${id_tasks}">
+                    <div class='tasks' style="height:${height_1}px" id="task_${id_tasks}" draggable='true' ondragstart='onDragStart(event);'>
                     <textarea class="task_textarea" rows="4" cols="50" readonly="true" maxlength="88">${input_task.val()}</textarea>
                         <button type="button" id="button_delete_${id_tasks}" 
-                            class="delete_task" onclick="deleteTask(${id_tasks})">
+                            class="delete_task" onclick="deleteTask(${id_tasks},false)">
                             <i class="fas fa-trash"></i>
                         </button>
                     </div>
@@ -37,13 +38,14 @@ function addTask() {
             `);
             // Increment counter for next id
             id_tasks++;
-        } else { 
+        } else {
+            tasks_2++;
             $("#task_list_2").append(`
                 <li>
-                    <div class='tasks_2' style="height:${height_2}px"" id="task_${id_tasks}">
+                    <div class='tasks_2' style="height:${height_2}px"" id="task_${id_tasks_2}_2" draggable='true' ondragstart='onDragStart(event);'>
                         <textarea class="task_textarea_2" rows="4" cols="50" readonly="true" maxlength="88">${input_task.val()}</textarea>
-                        <button type="button" id="button_delete_${id_tasks}" 
-                            class="delete_task" onclick="deleteTask(${id_tasks})">
+                        <button type="button" id="button_delete_${id_tasks_2}" 
+                            class="delete_task" onclick="deleteTask(${id_tasks_2},true)">
                             <i class="fas fa-trash"></i>
                         </button>
                     </div>
@@ -58,22 +60,28 @@ function addTask() {
     }
 
     // Show/Hide label about empty list
-    emptyList(tasks);
+    emptyList(tasks, tasks_2);
 }
 
-function deleteTask(id_task) {
-    tasks--;
+function deleteTask(id_task, kanban) {
+    (kanban) ? tasks_2-- : tasks--;
+
+    let task_selected;
+
     // Identify the task
-    task_selected = $(`#task_${id_task}`);
+    (kanban) ? task_selected = $(`#task_${id_task}_2`) : task_selected = $(`#task_${id_task}`);
 
     // Remove selected task
     task_selected.remove();
 
     // Show/Hide label about empty list
-    emptyList(tasks);
+    emptyList(tasks, tasks_2);
+
 }
 
-function emptyList(tasks) {
+function emptyList(tasks_1, tasks_2) {
     let nothing_to_do = $("#nothing_to_do");
-    (tasks > 0) ? nothing_to_do.addClass('hide') : nothing_to_do.removeClass('hide');
+    let nothing_to_do_2 = $('#nothing_to_do_2');
+    (tasks_1 > 0) ? nothing_to_do.addClass('hide') : nothing_to_do.removeClass('hide');
+    (tasks_2 > 0) ? nothing_to_do_2.addClass('hide') : nothing_to_do_2.removeClass('hide');
 }
